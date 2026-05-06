f57c7843b94c52e785f496ea519d7044c9304883
shinonome-MiDUki <shiinaayame3@gmail.com>
2026-05-06 19:36:04 +0900

    Debugged on Maya

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import json
+import re
 
 from PySide6.QtWidgets import (
     QVBoxLayout, QHBoxLayout, QLabel, 
@@ -13,7 +14,7 @@ from maya import mel as mel
 from maya.app.general.mayaMixin import MayaQWidgetDockableMixin
 
 unit_modelling_toolkit_obj_name = "UnitModellingToolkitWindow"
-unit_modelling_toolkit_data_file = Path("")
+unit_modelling_toolkit_data_file = Path("/Users/shiinaayame/Downloads/test_mytool.json")
 
 class MyCustomWindow(MayaQWidgetDockableMixin, QtWidgets.QDialog):
     def __init__(self, parent=None):
@@ -55,28 +56,44 @@ class MyCustomWindow(MayaQWidgetDockableMixin, QtWidgets.QDialog):
         layout.addLayout(sublayout1)
 
     def on_copy(self):
-        copy_name_dialog = cmds.promptDialog(
-            title = "Copying Object Name",
-            message = "Enter Object Name : ",
-            button = ["Confirm", "Cancel"],
-            defaultButton = "Confirm",
-            cancelButton = "Cancel",
-            dismissButton = "Cancel"
-        )
-        if copy_name_dialog == "Confirm":
-            copying_obj_name = str(cmds.promptDialog(q=True, text=True))
-            c = 1
-            root_name = copying_obj_name
-            while cmds.objExists(f"UnitModellingToolkitGP|{copying_obj_name}"):
-                copying_obj_name = f"{root_name}{c}"
-                c += 1
-        else:
+        def naming_dialog() -> str | None:
+            copy_name_dialog = cmds.promptDialog(
+                title = "Copying Object Name",
+                message = "Enter Object Name : ",
+                button = ["Confirm", "Cancel"],
+                defaultButton = "Confirm",
+                cancelButton = "Cancel",
+                dismissString = "Cancel"
+            )
+            if copy_name_dialog == "Confirm":
+                copying_obj_name = str(cmds.promptDialog(q=True, text=True))
+                return copying_obj_name
+            else:
+                return None
+            
+        copying_obj_name = naming_dialog() 
+        while re.match(r"[a-zA-Z_][a-zA-Z0-9_]*$", copying_obj_name) is None:
+            cmds.inViewMessage(amg='<span style="color:#FF0000;">Invalid Name !</span>', 
+                   pos='botRight', 
+                   fade=True)
+            copying_obj_name = naming_dialog()
+            if copying_obj_name is None:
+                break
+        if copying_obj_name is None:
             return
+        c = 1
+        root_name = copying_obj_name
+        while cmds.objExists(f"UnitModellingToolkitGP|{copying_obj_name}"):
+            copying_obj_name = f"{root_name}{c}"
+            c += 1
+
         selected_obj_raw = cmds.ls(sl=True)
-        if not selected_obj:
+        if not selected_obj_raw:
             return
         selected_obj = cmds.duplicate(selected_obj_raw)
-        selected_obj = cmds.group(selected_obj, world=True)
+        selected_obj_w = cmds.parent(selected_obj, world=True)
+        if selected_obj_w is not None:
+            selected_obj = selected_obj_w
         if len(selected_obj) > 1:
             copying_obj_name = copying_obj_name if copying_obj_name != "" else f"{copying_obj_name[0]}sGroup"
             cmds.group(selected_obj, name=copying_obj_name)
@@ -93,7 +110,7 @@ class MyCustomWindow(MayaQWidgetDockableMixin, QtWidgets.QDialog):
         if not cmds.objExists("UnitModellingToolkitGP"):
             cmds.group(empty=True, name="UnitModellingToolkitGP")
         cmds.setAttr("UnitModellingToolkitGP.visibility", False)
-        cmds.parent(copying_obj_name, "UnitModellingToolkitGP", relative=True)
+        cmds.parent(copying_obj_name, "UnitModellingToolkitGP")
 
         if "stored_items" in self.tool_data:
             self.tool_data["stored_items"].append(copying_obj_name)
@@ -113,21 +130,46 @@ class MyCustomWindow(MayaQWidgetDockableMixin, QtWidgets.QDialog):
             self.tool_data["stored_items"] = current_stored
         with open(unit_modelling_toolkit_data_file, "w", encoding="utf-8") as f:
             json.dump(self.tool_data, f)
+        self.dropdown.clear()
+        self.dropdown.addItems(current_stored)
 
     def on_place(self):
         selected_obj_ls = cmds.ls(sl=True)
         if not selected_obj_ls:
             return
         selected_obj = selected_obj_ls[0]
-        selected_pos = cmds.xform(selected_obj, q=True, rotatePivot=True)
+        selected_pos = cmds.xform(selected_obj, q=True, rotatePivot=True, worldSpace=True)
         using_obj = self.dropdown.currentText().strip()
         using_obj_name = f"UnitModellingToolkitGP|{using_obj}"
         if not cmds.objExists(using_obj_name):
+            cmds.inViewMessage(amg='<span style="color:#FF0000;">Object not exist</span>', 
+                   pos='botRight', 
+                   fade=True)
+            current_stored = self.tool_data.get("stored_items",[])
+            if using_obj in current_stored:
+                current_stored.remove(using_obj)
+                self.tool_data["stored_items"] = current_stored
+            with open(unit_modelling_toolkit_data_file, "w", encoding="utf-8") as f:
+                json.dump(self.tool_data, f)
+            self.dropdown.clear()
+            self.dropdown.addItems(current_stored)
             return
         using_obj_duplicate = cmds.duplicate(using_obj_name)
-        using_obj_duplicate = cmds.group(using_obj_duplicate, world=True)
+        if len(using_obj_duplicate) > 1:
+            using_obj_duplicate_real = []
+            for obj in using_obj_duplicate:
+                shape = cmds.listRelatives(obj, shapes=True, fullPath=True) or []
+                if not shape:
+                    using_obj_duplicate_real.append(obj)
+                    break
+            if not using_obj_duplicate_real:
+                print("Group not found")
+                return
+            using_obj_duplicate = using_obj_duplicate_real
+        using_obj_duplicate = cmds.parent(using_obj_duplicate, world=True)[0]
         cmds.setAttr(f"{using_obj_duplicate}.visibility", True)
-        cmds.move(0, 0, 0, using_obj_duplicate, rotatePivotRelative=True)
+        print(selected_pos)
+        cmds.move(selected_pos[0], selected_pos[1], selected_pos[2], using_obj_duplicate, rotatePivotRelative=True)
         cmds.makeIdentity(using_obj_duplicate, apply=True, normal=False,
                           rotate=True, scale=True, translate=True,
                           preserveNormals=True)
